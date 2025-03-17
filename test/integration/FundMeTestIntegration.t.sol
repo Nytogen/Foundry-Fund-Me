04259eba55e869c657088e1ad1f7b9f19dc1ccee
Jason Lui <lui.jason10@gmail.com>
2025-03-17 13:43:52 -0400

    Removed commented out code

--- test/integration/FundMeTestIntegration.t.sol
+++ test/integration/FundMeTestIntegration.t.sol
@@ -15,7 +15,6 @@ contract FundMeTestIntegration is Test {
     uint256 constant GAS_PRICE = 1;
 
     function setUp() external {
-        //fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
         vm.deal(USER, STARTING_BALANCE);
